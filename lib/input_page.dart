8780cb487e7dc3f33195eee270604fa231f49e14
Lani <lani.mortontran@movemedical.com>
2022-03-23 16:09:28 -0700

    124. [Dart] Final vs. Const

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const activeContainerColor = Color(0xFF1D1E33);
+const bottomContainerHeight = 80.0;
+const bottomContainerColor = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -19,28 +23,34 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableCard(Color(0xFF1D1E33)),
+                  child: ReusableCard(activeContainerColor),
                 ),
                 Expanded(
-                  child: ReusableCard(Color(0xFF1D1E33)),
+                  child: ReusableCard(activeContainerColor),
                 ),
               ],
             ),
           ),
           Expanded(
-            child: ReusableCard(Color(0xFF1D1E33)),
+            child: ReusableCard(activeContainerColor),
           ),
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableCard(Color(0xFF1D1E33)),
+                  child: ReusableCard(activeContainerColor),
                 ),
                 Expanded(
-                  child: ReusableCard(Color(0xFF1D1E33)),
+                  child: ReusableCard(activeContainerColor),
                 ),
               ],
             ),
+          ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
           ),
         ],
       ),
